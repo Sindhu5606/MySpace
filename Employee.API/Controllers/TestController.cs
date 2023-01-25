19e13325ff7aedc3e844c31b5d1a0be9c874612d
deepakgullipalli <deepakgullipalli@yahoo.com>
2023-01-25 20:56:56 +0530

    created dummy controller actions

---
+++ Employee.API/Controllers/TestController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Employee.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<TestController> _logger;
+
+        public TestController(ILogger<TestController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetTestForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPut(Name = "UpdateExisting")]
+        public string UpdateExisting(string id)
+        {
+            return "Updating it from 2 to 3";
+        }
+
+        [HttpPost(Name = "CreateNew")]
+        public string CreateNew(string name)
+        {
+            return "create a new item called {name}";
+        }
+    }
+}
